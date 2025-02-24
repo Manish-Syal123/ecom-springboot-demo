3c0e1d83b48061eb98e4939d66876f0a01d88948
Manish_Syal <manishsyal77@gmail.com>
2025-02-24 17:22:16 +0530

    UpdateProduct & DeleteProduct functionality added, using ecom-frontend-4

--- src/main/java/com/manish/ecom_proj_1/service/ProductService.java
+++ src/main/java/com/manish/ecom_proj_1/service/ProductService.java
@@ -3,6 +3,7 @@ package com.manish.ecom_proj_1.service;
 import com.manish.ecom_proj_1.model.Product;
 import com.manish.ecom_proj_1.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,5 +29,16 @@ public class ProductService {
         product.setImageType(imageFile.getContentType());
         product.setImageData(imageFile.getBytes());
         return repo.save(product);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageData(imageFile.getBytes());
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        return  repo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        repo.deleteById(id);
     }
 }
